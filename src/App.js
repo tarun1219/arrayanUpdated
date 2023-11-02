1e300ccbc33c0933c5d1584540ee5c25f04fb5ed
tarun1219 <41871545+tarun1219@users.noreply.github.com>
2023-11-02 02:53:40 -0700

    Added SignUp page and auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container } from "react-bootstrap";
+import UserSignUp from "./components/UserSignUp";
+import Login from "./pages/Login";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // <Container 
+    //   className="d-flex align-items-center"
+    //   style={{minHeight: "100vh"}}
+    // >
+    //   <div className="w-100" style={{maxWidth: '400px'}}>
+    //   <UserSignUp />
+    //   </div>
+      
+    // </Container>
+    <Login />
+  
+  )
 }
 
 export default App;
